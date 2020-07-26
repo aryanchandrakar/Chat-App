8bcf0c933ed2e4ff581ba1816e89b24f59e31454
Aryan Chandrakar <aryan.c.2000@gmail.com>
2020-07-26 12:26:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/vitmeet/loginregistration.java
@@ -0,0 +1,41 @@
+package com.example.vitmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class loginregistration extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginregistration);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginregistration.this, login.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginregistration.this, registration.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
